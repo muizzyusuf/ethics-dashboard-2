16214661f793dbf333fb9cf4dbd0288bb60f1735
Deka Abdi <dekahassan16@gmail.com>
2022-02-11 14:53:11 -0800

    Deleted old blade files
    These are old blade files

--- ethicsdashboard/resources/views/student/deontologysection.blade.php
+++ ethicsdashboard/resources/views/student/deontologysection.blade.php
@@ -36,7 +36,77 @@
             imperatives (must do’s that are not optional).  To begin, 
             consider the reasons supporting each option. </p>
 
-      
+            @for($i=0; $i<count($options); $i++)
+            <div class="card-body">
+                <form method="POST" action="{{route('consequence.store')}}">
+                    {{ csrf_field() }}
+                    {{method_field('POST')}}
+                    <input type="hidden" id="id" name="id" value="{{$deontologySection->id}}" >
+
+                    <div class="form-group">
+                        <label class="font-weight-bold" for="option">Option {{$i+1}}</label>
+                        <input type="hidden" id="option_id" name="option_id" @if(isset($options[$i])) value="{{$options[$i]->id}}" @endif>
+                        <textarea class="form-control" id="option" name="option" rows="1" readonly>@if(count($options)>0) {{$options[$i]->option}} @endif </textarea>
+                    </div>
+                    
+                    @if(count($options[$i]->consequences)>0 )
+                        @foreach($consequences as $consequence)
+                            @if(($consequence->option_id == $options[$i]->id) && ($consequence->type == 'short'))
+                                <div class="form-group">
+                                    <label class="font-weight-bold" for="short">Motivation</label>
+                                    <input type="hidden" id="short_id" name="short_id" value="{{$consequence->id}}">
+                                    <input type="checkbox" name="fruits[]" value="Mango">Serves your interests <br/>
+                                </div>
+
+                            @elseif(($consequence->option_id == $options[$i]->id) && ($consequence->type == 'long'))
+                                <div class="form-group">
+                                    <label class="font-weight-bold" for="long">Decision</label>
+                                    <input type="hidden" id="long_id" name="long_id" value="{{$consequence->id}}">
+                                    <textarea class="form-control" id="long" name="long" rows="3" required>{{$consequence->consequence}}</textarea>
+                                </div>
+                            @endif
+                            
+                        @endforeach
+                        <div class="form-group">
+                            <input type="submit" class="float-right btn btn-primary" value="Save">
+                        </div>
+                    @else
+                    
+                        <div class="form-group">
+                            <label class="font-weight-bold" for="motivation">What is your motivation?</label><br/>
+                            <input type="checkbox" name="motivations">Serves your interests <br/>
+                            <input type="checkbox" name="motivations">Serves the interests of someone else you want to impress <br/>
+                            <input type="checkbox" name="motivations">It will look good <br/>
+                            <input type="checkbox" name="motivations">It will pay off in the long run<br/>
+                            <input type="checkbox" name="motivations">It costs very little <br/>
+                            <input type="checkbox" name="motivations">It’s the right thing to do<br/>
+                            <input type="checkbox" name="motivations">Other:
+                            <textarea class="form-control" id="short" name="short" rows="1"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="font-weight-bold" for="Decision">Decision</label>
+                            <p> This reasoning is consistent with 
+                            <select name="imperatives" id="imperatives">
+                                <option value="Hypothetical">Hypothetical</option>
+                                <option value="Categorical">Categorical</option>
+                            </select>reasoning.Therefore
+                            <select name="decision" id="decision">
+                                <option value="....">cannot</option>
+                                <option value="may">may</option>
+                            </select>support a moral action.</p>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="float-right btn btn-primary" value="Save">
+                        </div>
+                
+                    @endif
+                    
+
+                </form>
+            </div>
+        @endfor
     </div>
 
 </div>
