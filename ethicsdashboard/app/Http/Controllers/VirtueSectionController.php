ccbd5d036aca6528587c7f363277af25740c8e3e
Radhi1017 <radhika1017@yahoo.com>
2022-02-11 18:00:03 -0800

    modified controller's show method

--- ethicsdashboard/app/Http/Controllers/VirtueSectionController.php
+++ ethicsdashboard/app/Http/Controllers/VirtueSectionController.php
@@ -4,6 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\VirtueSection;
 use Illuminate\Http\Request;
+use App\Models\Virtue;
+use App\Models\Dashboard;
+use App\Models\EthicalIssue;
+use App\Models\CaseStudy;
+use App\Models\Option;
+use App\Models\Stakeholder;
+use App\Models\StakeholderSection;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 
 class VirtueSectionController extends Controller
 {
@@ -44,10 +55,45 @@ class VirtueSectionController extends Controller
      * @param  \App\Models\VirtueSection  $virtueSection
      * @return \Illuminate\Http\Response
      */
-    public function show(VirtueSection $virtueSection)
+    public function show($id)
     {
+        //TODO
+        $virtueSection = VirtueSection::where('id', $id)->first();
+        $dashboard = Dashboard::where('id',$virtueSection->dashboard->id)->first();        
+        $ethicalissue = EthicalIssue::where('id', $dashboard->ethical_issue_id)->first();
+        $casestudy = CaseStudy::where('id', $dashboard->case_study_id)->first();
+        $stakeholders = Stakeholder::where('stakeholder_section_id', $dashboard->stakeholder_section_id)->get();
+        $options = Option::where('ethical_issue_id', $ethicalissue->id)->get();   
         
+
+        // for each option assign a virtue_id (so assigbn general id of the virtue to the options)
+        //make arrays of virtues outside then 
+        $virtues = []; 
+        foreach($options as $option){
+            $virtue = new Virtue;
+            array_push($virtues, $virtue);
+            $option -> virtue_id = $virtue -> id;
+        }
+
+      
+        if(Auth::user()->role()->first()->id == 3){
+            return view('student.virtueethics')->with('dashboard', $dashboard)
+                                ->with('ethicalissue', $ethicalissue)
+                                ->with('stakeholders', $stakeholders)
+                                ->with('casestudy', $casestudy)
+                                ->with('options', $options)
+                                ->with('virtueSection', $virtueSection)
+                                ->with('virtues', $virtues);
+
+        }else{
+            return view('virtueethics')->with('dashboard', $dashboard)
+                                ->with('ethicalissue', $ethicalissue)
+                                ->with('stakeholders', $stakeholders)
+                                ->with('casestudy', $casestudy)
+                                ->with('options', $options)
+                                ->with('virtueSection', $virtueSection);
     }
+}
 
     /**
      * Show the form for editing the specified resource.
@@ -84,3 +130,4 @@ class VirtueSectionController extends Controller
         //
     }
 }
+
