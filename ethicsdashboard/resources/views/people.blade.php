c5f0d08fef8f992546a0669a98cf1e9c07e1b978
Muizz <abdelmuizz.yusuf@hotmail.com>
2022-04-02 16:07:03 -0700

    people UI fixed

--- ethicsdashboard/resources/views/people.blade.php
+++ ethicsdashboard/resources/views/people.blade.php
@@ -18,13 +18,38 @@
     <div class="container">
 
         <div class="jumbotron">
+
+            <div class="row my-3">
+                <div class="col">
+                    <h1 class="display-5 font-weight-bold text-left">Everyone</h1>
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col">
+                            <div class="text-right">
+                                <!-- add student Button trigger modal -->
+                                <button type="button" class="btn btn-primary" style="width:180px" data-toggle="modal" data-target="#addStudentModal">
+                                    Add Student
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="text-right">
+                                <!-- add student Button trigger modal -->
+                                <button type="button" class="btn btn-primary mt-1" style="width:180px" data-toggle="modal" data-target="#addTAModal">
+                                    Add Teaching Assistant
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             
-            <h1 class="display-5 font-weight-bold float-left">Everyone</h1>
-            
-            <!-- add student Button trigger modal -->
-            <button type="button" class="ml-2 btn btn-primary float-right" data-toggle="modal" data-target="#addStudentModal">
-                Add Student
-            </button>
 
             <!-- add student Modal -->
             <div class="modal fade" id="addStudentModal" tabindex="-1" aria-labelledby="addStudentModalLabel" aria-hidden="true">
@@ -79,10 +104,7 @@
                 </div>
             </div>
 
-            <!-- add student Button trigger modal -->
-            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#addTAModal">
-                Add Teaching Assistant
-            </button>
+            
 
             <!-- add student Modal -->
             <div class="modal fade" id="addTAModal" tabindex="-1" aria-labelledby="addTAModalLabel" aria-hidden="true">
@@ -140,63 +162,65 @@
 
             @if(count($people)>0)
 
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Role</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                        @foreach($people as $person)
-
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover table-bordered">
+                        <thead>
                             <tr>
-                                <th scope="row">{{$person->name}}</th>
-                                <td>{{$person->email}}</td>
-                                <td>{{$person->role}}</td>
-                                <td>
-                                    @if($person->role_id !== 1)
-                                        <!-- remove user from course trigger modal -->
-                                        <button type="button" class="btn btn-danger float-right" data-toggle="modal" data-target="#removeUser{{$person->id}}Modal">
-                                            Remove
-                                        </button>
-                                        
-                                        <!-- remove user from course Modal -->
-                                        <div class="modal fade" id="removeUser{{$person->id}}Modal" tabindex="-1" role="dialog" aria-labelledby="removeUser{{$person->id}}ModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog modal-sm" role="document">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="removeUser{{$person->id}}ModalLabel">Remove User from Course</h5>
-                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Role</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            @foreach($people as $person)
+    
+                                <tr>
+                                    <th scope="row">{{$person->name}}</th>
+                                    <td>{{$person->email}}</td>
+                                    <td>{{$person->role}}</td>
+                                    <td>
+                                        @if($person->role_id !== 1)
+                                            <!-- remove user from course trigger modal -->
+                                            <button type="button" class="btn btn-danger float-right" data-toggle="modal" data-target="#removeUser{{$person->id}}Modal">
+                                                ✖
+                                            </button>
+                                            
+                                            <!-- remove user from course Modal -->
+                                            <div class="modal fade" id="removeUser{{$person->id}}Modal" tabindex="-1" role="dialog" aria-labelledby="removeUser{{$person->id}}ModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-sm" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="removeUser{{$person->id}}ModalLabel">Remove User from Course</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+    
+                                                        <form action="{{route('courseuser.destroy', [$course->id, $person->id])}}" method="POST">
+                                                            {{ csrf_field() }}
+                                                            {{method_field('DELETE')}}
+                                                            
+                                                            <div class="modal-body">
+                                                                Are you sure you want to remove this user from this course?
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                                                <button type="submit" class="btn btn-danger">Yes</button>
+                                                            </div>
+                                                        </form>
                                                     </div>
-
-                                                    <form action="{{route('courseuser.destroy', [$course->id, $person->id])}}" method="POST">
-                                                        {{ csrf_field() }}
-                                                        {{method_field('DELETE')}}
-                                                        
-                                                        <div class="modal-body">
-                                                            Are you sure you want to remove this user from this course?
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                                                            <button type="submit" class="btn btn-danger">Yes</button>
-                                                        </div>
-                                                    </form>
                                                 </div>
                                             </div>
-                                        </div>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    
-                    </tbody>
-                </table>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        
+                        </tbody>
+                    </table>
+                </div>
 
             @endif
         </div>
